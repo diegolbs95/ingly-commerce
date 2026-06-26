e1e6272e2a17e73d38f27ef65ff0d38243d7ddf2
Diego Luis <diego_bezerra95@hotmail.com>
2026-06-26 00:34:43 -0300

    Montando estrutura de loyout

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Ingly Commerce",
+  description: "Moda feminina premium.",
+  locale: "pt-BR",
+  author: "Ingly Jeans",
+
+  url: "https://inglycommerce.com.br",
+
+  links: {
+    instagram: "",
+    whatsapp: "",
+  },
+} as const;
